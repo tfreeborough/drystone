f80be0ee1f84764abe5c6e35111707915c6a6e94
Tom Freeborough <tom@harewood.io>
2024-10-07 19:55:11 +0100

    Update application.types.ts

--- types/application.types.ts
+++ types/application.types.ts
@@ -1,4 +1,15 @@
-import { JSONContent } from '@tiptap/react';
+export type JSONContent = {
+  type?: string;
+  attrs?: Record<string, any>;
+  content?: JSONContent[];
+  marks?: {
+    type: string;
+    attrs?: Record<string, any>;
+    [key: string]: any;
+  }[];
+  text?: string;
+  [key: string]: any;
+};
 
 export interface ApplicationAuthor {
   name: string;
