35f05b53dd831020077eab94722a653bd2ed381f
Tom Freeborough <tom@harewood.io>
2024-12-21 15:43:01 +0000

    build fix

--- editor/src/components/organisms/SceneEditor/SceneEditor.tsx
+++ editor/src/components/organisms/SceneEditor/SceneEditor.tsx
@@ -1,14 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import { v4 } from 'uuid';
-import { generateText } from '@tiptap/react';
 import { Reorder } from 'framer-motion';
-import Document from '@tiptap/extension-document';
-import Paragraph from '@tiptap/extension-paragraph';
-import Text from '@tiptap/extension-text';
-import HardBreak from '@tiptap/extension-hard-break';
-import HeadingNode from '@tiptap/extension-heading';
-import BulletList from '@tiptap/extension-bullet-list';
-import ListItem from '@tiptap/extension-list-item';
 
 import { Choice as ChoiceType, Frame, Scene } from '@shared/types';
 import TextInput from '../../atoms/TextInput/TextInput.tsx';
